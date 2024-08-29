090a089d1f7a659141afca03d73c7b77c914affd
Shikho <shikho@Shikhos-MacBook-Pro.local>
2024-08-29 17:03:56 +0600

    custom types added

---
+++ backend/src/types/custom.d.ts
@@ -0,0 +1,8 @@
+declare module 'cookie-parser';
+declare module 'cors';
+declare module 'express-session';
+declare module 'swagger-jsdoc';
+declare module 'swagger-ui-express';
+declare module 'morgan';
+declare module 'jsonwebtoken';
+declare module 'bcrypt';
