1d4a4b52712611cb4d64cfa803fd42cd4246dc81
Shikho <shikho@Shikhos-MacBook-Pro.local>
2024-08-29 18:13:22 +0600

    cors updated

--- backend/src/utils/api-utils/destroyToken.ts
+++ backend/src/utils/api-utils/destroyToken.ts
@@ -7,7 +7,7 @@ const destroyToken = (res: Response) => {
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict" as const,
+    sameSite: "none" as const,
     maxAge: 0, // Set maxAge to 0 milliseconds to expire the cookie immediately
     path: '/', // Ensure path is set
   };
